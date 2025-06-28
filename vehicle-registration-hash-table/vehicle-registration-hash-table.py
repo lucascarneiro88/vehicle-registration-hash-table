80ed28649dfca142cbf5a9679e88f93d9636b861
Lucas <carneirolucas88@gmail.com>
2025-06-28 12:54:57 -0300

    feat: implement initial hash table structure and State class

---
+++ vehicle-registration-hash-table/vehicle-registration-hash-table.py
@@ -0,0 +1,13 @@
+# Exigência de código 1 de 7
+# implementar a tabela Hash com 10 posições
+# onde inicialmente todas as posições possuem valor None
+
+class States:
+    def __init__(self, state_name, state_code):
+        self.state.name = state_name
+        self.state.code = state_code
+        self.current = None
+
+
+# Exigência de código 2 de 7
+
